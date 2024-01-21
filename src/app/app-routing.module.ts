3c3200d26420ce68440b92b61d9636fd87fda6f2
sarah <sarah01.mok@gmail.com>
2024-01-22 00:51:23 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { AllClubsComponent } from './pages/all-clubs/all-clubs.component';
+import { EventsComponent } from './pages/events/events.component';
+import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { CommentFormComponent } from './comments/comment-form/comment-form.component';
+import { CommentListComponent } from './comments/comment-list/comment-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path :'' , component: HomeComponent},
+{path :'post' , component:SinglePostComponent},
+{path :'contact' , component:ContactUsComponent},
+{path :'club' , component:AllClubsComponent},
+{path :'events' , component:EventsComponent},
+{path :'term-conditions' , component:TermsAndConditionsComponent},
+{path :'footer' , component:FooterComponent},
+{path :'header' , component:HeaderComponent},
+{path :'navbar' , component:NavbarComponent},
+{path :'comment-form' , component:CommentFormComponent},
+{path :'comment-list' , component:CommentListComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { EventsComponent } from './pages/events/events.component';
+import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { AllClubsComponent } from './pages/all-clubs/all-clubs.component';
+import { CommentFormComponent } from './comments/comment-form/comment-form.component';
+import { CommentListComponent } from './comments/comment-list/comment-list.component';
+import { PostCardComponent } from './layouts/post-card/post-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    SinglePostComponent,
+    EventsComponent,
+    TermsAndConditionsComponent,
+    ContactUsComponent,
+    AllClubsComponent,
+    CommentFormComponent,
+    CommentListComponent,
+    PostCardComponent
   ],
   imports: [
     BrowserModule,
